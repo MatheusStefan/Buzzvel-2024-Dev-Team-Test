0ecf0ffa66685dfd8fe7f7c7814659c4db4abcd3
Matheus Stefan <roccomatheus0@gmail.com>
2024-08-26 00:17:36 -0300

    header, footer and hero sections

---
+++ Buzzvel project/src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,79 @@
+import React, { useRef } from "react";
+import styles from "./LandingPage.module.scss";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger, TextPlugin } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, TextPlugin);
+
+type LandingPageProps = unknown;
+
+const LandingPage: React.FC<LandingPageProps> = () => {
+  const heroRef = useRef<HTMLDivElement | null>(null);
+  const contactRef = useRef<HTMLParagraphElement | null>(null);
+  const taglineRef = useRef<HTMLSpanElement | null>(null);
+
+  useGSAP(() => {
+    const tl = gsap.timeline({ repeat: 0 });
+    tl.to(heroRef.current, {
+      backgroundColor: "rgb(255, 255, 255, 0.8)",
+      scrollTrigger: {
+        trigger: ".test",
+        start: "top top",
+        end: "center bottom",
+        scrub: true,
+      },
+    });
+    tl.to(contactRef.current, {
+      color: "#fff",
+    });
+    gsap.to(taglineRef.current, {
+      duration: 3,
+      text: {
+        value: "Changing the WORLD through CODE",
+        type: "diff",
+      },
+      repeat: 5,
+    });
+  }, []);
+
+  return (
+    <section className={styles.landingPage} id="home">
+      <div className={styles.introduction}>
+        <h1 className={styles["introduction-title"]}>Matheus Stefan</h1>
+        <p className={styles.description}>Frontend developer</p>
+        <span className={styles["hero-section__tagline"]} ref={taglineRef}>
+          Changing the world through code
+        </span>
+      </div>
+      <div className={styles["hero-section"]} ref={heroRef}>
+        <div className={styles["hero-section__title"]}>
+          <img
+            src="src/assets/user.png"
+            alt="use-picture"
+            className={styles.user}
+          />
+          <p className={styles["hero-section__contact"]} ref={contactRef}>
+            <a
+              href="mailto:roccomatheus0@gmail.com"
+              className={styles["hero-section__contact-info"]}
+            >
+              Send me a message!
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className={styles.test} id="about-me">
+        about-me
+      </div>
+      <div className={styles.test} id="projects">
+        projects
+      </div>
+      <div className={styles.test} id="timeline">
+        timeline
+      </div>
+    </section>
+  );
+};
+
+export default LandingPage;
